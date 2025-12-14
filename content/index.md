f32c035668df949f8c6f56a2a464fcab8ab38f62
Nayrae <nayrae@users.noreply.github.com>
2025-12-14 23:17:42 +0100

    Quartz sync: Dec 14, 2025, 11:17 PM

--- content/index.md
+++ content/index.md
@@ -2,7 +2,7 @@
 title: Nayrae
 publish: true
 ---
-a
+·a
 This is a blank Quartz installation.
 See the [documentation](https://quartz.jzhao.xyz) for how to get started.
 whoamim
--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -35,9 +35,6 @@ export default {
     explorer: {
       title: "Explorer",
     },
-    footer: {
-      createdWith: "Created with",
-    },
     graph: {
       title: "Graph View",
     },
